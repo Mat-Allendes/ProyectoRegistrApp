b52f84d8b69818bdf3264afdcb30c9b5a56c5806
Seba-Solar <101576567+Seba-Solar@users.noreply.github.com>
2022-10-02 21:03:19 -0300

    Subo mi parte del trabajo LOGIN y REGISTER
    v.1.0

---
+++ login/login.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  formularioLogin: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController) {
+
+    this.formularioLogin = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  async ingresar(){
+    console.log('Boton Ingresar Funciona')
+    var f = this.formularioLogin.value;
+    var usuario = JSON.parse(localStorage.getItem('usuario'));
+    if(usuario.nombre == f.nombre && usuario.password == f.password){
+      console.log('Ingresado');
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Alerta',
+        message: 'Tienes un error en los campos!!',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+    }
+  }
+}
